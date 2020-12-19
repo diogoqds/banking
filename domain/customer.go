91f3ccd2c743f4c2850444c9f10f36520d8dfacb
Diogo Queiroz <diogo.qds97@gmail.com>
2020-12-19 19:18:21 -0300

    refactor: use customer dto

--- domain/customer.go
+++ domain/customer.go
@@ -1,17 +1,39 @@
 package domain
 
-import "github.com/diogoqds/banking/errs"
+import (
+	"github.com/diogoqds/banking/dto"
+	"github.com/diogoqds/banking/errs"
+)
 
 type Customer struct {
-	Id          string `json:"id" db:"customer_id"`
-	Name        string `json:"name"`
-	City        string `json:"city"`
-	Zipcode     string `json:"zipcode"`
-	DateOfBirth string `json:"date_of_birth" db:"date_of_birth"`
-	Status      string `json:"status"`
+	Id          string `db:"customer_id"`
+	Name        string
+	City        string
+	Zipcode     string
+	DateOfBirth string `db:"date_of_birth"`
+	Status      string
 }
 
 type CustomerRepository interface {
 	FindAll(status string) ([]Customer, *errs.AppError)
 	FindById(string) (*Customer, *errs.AppError)
+}
+
+func (c Customer) statusAsText() string {
+	statusAsText := "active"
+	if c.Status == "0" {
+		statusAsText = "inactive"
+	}
+	return statusAsText
+}
+
+func (c Customer) ToDto() dto.CustomerResponse {
+	return dto.CustomerResponse{
+		Id:          c.Id,
+		Name:        c.Name,
+		City:        c.City,
+		Zipcode:     c.Zipcode,
+		DateOfBirth: c.DateOfBirth,
+		Status:      c.statusAsText(),
+	}
 }
---
+++ dto/customer_response.go
@@ -0,0 +1,10 @@
+package dto
+
+type CustomerResponse struct {
+	Id          string `json:"id"`
+	Name        string `json:"name"`
+	City        string `json:"city"`
+	Zipcode     string `json:"zipcode"`
+	DateOfBirth string `json:"date_of_birth"`
+	Status      string `json:"status"`
+}
